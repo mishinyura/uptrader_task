4d249a634b01f7310ecb28e3672923ff2c6c4816
yuramishin <robothot@yandex.ru>
2024-07-23 16:09:36 +0300

    add model Link

--- django_app/my_app/models.py
+++ django_app/my_app/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Menu(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
+
+class Link(models.Model):
+    parent = models.ForeignKey('Link', on_delete=models.CASCADE, null=True, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    url = models.CharField(max_length=255, null=False, blank=True)
