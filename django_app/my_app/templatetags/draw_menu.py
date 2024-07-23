a23d59171e80b0f25ab4c2318c32c5f3d34457f9
yuramishin <robothot@yandex.ru>
2024-07-23 22:09:41 +0300

    Внес правки в шаблон, стили + добавил нижнее меню

--- django_app/my_app/templatetags/draw_menu.py
+++ django_app/my_app/templatetags/draw_menu.py
@@ -3,11 +3,6 @@ from my_app.models import Menu, Link
 
 register = template.Library()
 
-@register.simple_tag()
-def dra_menu():
-    data = Menu.objects.filter(id=1).prefetch_related('link')
-    return data
-
 @register.inclusion_tag('my_app/list_menu.html')
 def draw_menu(filter: str) -> dict:
     queryset = Menu.objects.get(name=filter).link.order_by('id')
