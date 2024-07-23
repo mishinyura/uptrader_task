889d3e10224b859050d6673147841b733e03cbcb
yuramishin <robothot@yandex.ru>
2024-07-23 16:23:53 +0300

    добавлена верстка

--- django_app/my_app/models.py
+++ django_app/my_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Menu(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
+    link = models.ManyToManyField('Link')
 
 class Link(models.Model):
     parent = models.ForeignKey('Link', on_delete=models.CASCADE, null=True, blank=False)
