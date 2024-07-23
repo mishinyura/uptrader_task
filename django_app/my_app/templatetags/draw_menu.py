fcc79057e754036931ee4de80eee765fd1a7fee2
yuramishin <robothot@yandex.ru>
2024-07-23 19:53:06 +0300

    Добавлены шаблоны, реализовано отображение меню

--- django_app/my_app/templatetags/draw_menu.py
+++ django_app/my_app/templatetags/draw_menu.py
@@ -4,7 +4,11 @@ from my_app.models import Menu, Link
 register = template.Library()
 
 @register.simple_tag()
-def get_menu():
+def dra_menu():
     data = Menu.objects.filter(id=1).prefetch_related('link')
-    print()
     return data
+
+@register.inclusion_tag('my_app/list_menu.html')
+def draw_menu(filter: str) -> dict:
+    queryset = Menu.objects.get(name=filter).link.order_by('parent')
+    return {'queryset': queryset, 'menu': filter}
