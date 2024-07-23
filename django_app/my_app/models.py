0c705e9f4b01fcea63fa37fe42ad5eb1359b67e0
yuramishin <robothot@yandex.ru>
2024-07-23 19:58:52 +0300

    Корректировка архитектуры бд

--- django_app/my_app/models.py
+++ django_app/my_app/models.py
@@ -8,3 +8,4 @@ class Link(models.Model):
     parent = models.ForeignKey('Link', on_delete=models.CASCADE, null=True, blank=False)
     name = models.CharField(max_length=100, null=False, blank=False)
     url = models.CharField(max_length=255, null=False, blank=True)
+    position = models.PositiveSmallIntegerField(null=False, blank=True)
