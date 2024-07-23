d24be951ecce01e6c6ce66006732730c27835ae9
yuramishin <robothot@yandex.ru>
2024-07-23 15:50:45 +0300

    добавлена вью функция, внесены правки в адресацию

--- django_app/my_app/views.py
+++ django_app/my_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def showIndex(request: HttpRequest) -> HttpResponse:
+    data = {
+        'test': 'test'
+    }
+    return render(request, 'my_app/index.html', data)
